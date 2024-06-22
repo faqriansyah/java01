5291165537c5423fb51546efa0f0d52e7ecd6220
faqriansyah <ifaqriansyah@gmail.com>
2024-06-22 19:46:49 +0800

    adding new linkedhashset file

---
+++ classes/LinkedHashSett.java
@@ -0,0 +1,37 @@
+package classes;
+
+import java.util.*;
+
+public class LinkedHashSett {
+    public static void main(String[] args) {
+        List<String> buah0 = new ArrayList<>();
+        buah0.add("Kelengkeng");
+        buah0.add("Rambutan");
+        LinkedHashSet<String> buah = new LinkedHashSet<>(buah0);
+        buah.add("Pepaya");
+        buah.add("Semangka");
+        buah.add("Semangka");
+        buah.add("Mangga");
+        System.out.println(buah.isEmpty());
+        System.out.println(buah.size());
+
+        Separator.xsp();
+
+        System.out.println("Iteration");
+        Iterator<String> iterator1 = buah.iterator();
+        while (iterator1.hasNext()) {
+            System.out.println(iterator1.next());
+        }
+
+        Separator.xsp();
+
+        System.out.println("remove if");
+        buah.removeIf(x->x.contains("Mangga"));
+        System.out.println(buah);
+
+        Separator.xsp();
+
+        Spliterator<String> spliterator = buah.spliterator();
+        spliterator.forEachRemaining(System.out::println);
+    }
+}
